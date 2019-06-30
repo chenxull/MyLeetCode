7030b15f7dbe327db51b90281d8aa0cfa202131f
chenxull <cx94758428042@gmail.com>
2019-06-30 19:24:20 +0800

    剑指 offer45

--- 2019.6.28/45/ac45.go
+++ 2019.6.28/45/ac45.go
@@ -1,2 +1,34 @@
 package offer45
- 
+
+import (
+	"sort"
+	"strconv"
+)
+
+type intSlice []int
+
+func (p intSlice) Less(i, j int) bool {
+	return strconv.Itoa(p[i])+strconv.Itoa(p[j]) < strconv.Itoa(p[j])+strconv.Itoa(p[i])
+}
+
+func (p intSlice) Len() int {
+	return len(p)
+}
+
+func (p intSlice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func printMinNumber(number []int) string {
+	if number == nil {
+		return ""
+	}
+
+	sort.Sort(intSlice(number))
+	var ans string
+	for _, value := range number {
+		ans += strconv.Itoa(value)
+	}
+	//fmt.Println(ans)
+	return ans
+}
--- 2019.6.28/45/ac45_test.go
+++ 2019.6.28/45/ac45_test.go
@@ -1,1 +1,24 @@
 package offer45
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var tsc = []struct {
+	input []int
+	ans   string
+}{
+	{
+		[]int{3, 32, 321},
+		"321323",
+	},
+}
+
+func Test_ac45(t *testing.T) {
+	ast := assert.New(t)
+	for _, ts := range tsc {
+		ast.Equal(ts.ans, printMinNumber(ts.input))
+	}
+
+}

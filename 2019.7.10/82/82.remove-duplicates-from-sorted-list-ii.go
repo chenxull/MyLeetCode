9e7cbd52f585363bbc44596a520b372b48d60535
chenxull <cx94758428042@gmail.com>
2019-07-11 00:34:18 +0800

    leetcode 82

--- 2019.7.10/82/82.remove-duplicates-from-sorted-list-ii.go
+++ 2019.7.10/82/82.remove-duplicates-from-sorted-list-ii.go
@@ -12,6 +12,30 @@ package problem82
  *     Next *ListNode
  * }
  */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//  只要处理的思路清晰，这种题目还是很容易的
 func deleteDuplicates(head *ListNode) *ListNode {
+	dumyHead := ListNode{Next: head}
+
+	cur := head
+	left := &dumyHead
+	for cur != nil && cur.Next != nil {
+		if cur.Val != cur.Next.Val {
+			left = cur
+			cur = cur.Next
+		} else {
+			for cur.Next != nil && cur.Val == cur.Next.Val {
+				cur = cur.Next
+			}
+			left.Next = cur.Next
+			cur = cur.Next
+		}
+	}
+	return dumyHead.Next
 
 }

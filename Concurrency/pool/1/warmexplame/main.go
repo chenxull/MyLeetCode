a51be875ef61321639ccb3dc47dec5820da42f79
chenxull <cx94758428042@gmail.com>
2019-07-29 00:28:46 +0800

    change filename and Add  Pool Example

---
+++ Concurrency/pool/1/warmexplame/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var numCalcsCreated int
+	calcPool := &sync.Pool{
+		New: func() interface{} {
+			numCalcsCreated += 1
+			mem := make([]byte, 1024)
+			return &mem // 1
+		},
+	}
+
+	// 将池扩充到4KB
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	const numWorkers = 1024 * 1024
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	for i := numWorkers; i > 0; i-- {
+		go func() {
+			defer wg.Done()
+
+			mem := calcPool.Get().(*[]byte) // 2
+			defer calcPool.Put(mem)
+
+		}()
+	}
+
+	// 假设内存中执行了一些快速的操作
+
+	wg.Wait()
+	fmt.Printf("%d calculators were created.", numCalcsCreated)
+}
